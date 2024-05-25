7cd2205bbf72cc9ff5cbd9ae92a6deebdfbc08b1
Ailson12 <robertofeitosa06@gmail.com>
2024-05-25 17:36:04 -0300

    feat: saving the user's favorite theme

--- src/store/theme.store.ts
+++ src/store/theme.store.ts
@@ -1,16 +1,21 @@
 import { ThemeEnum } from "@/enums/ThemeEnum";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type State = {
   theme: ThemeEnum;
   setTheme(value: ThemeEnum): void;
 };
 
-export const useThemeStore = create<State>((set) => ({
-  theme: ThemeEnum.DARK,
-  setTheme(value) {
-    set({
-      theme: value,
-    });
-  },
-}));
+export const useThemeStore = create(
+  persist<State>((set) => ({
+    theme: ThemeEnum.DARK,
+    setTheme(value) {
+      set({
+        theme: value,
+      });
+    },
+  }), {
+    name: 'theme-country'
+  })
+);
